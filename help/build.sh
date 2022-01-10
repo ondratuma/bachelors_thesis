78995c7d12c5d8efb74413e1170861c5d79f6e88
Ondřej Tůma <tumaondra2000@gmail.com>
2022-01-10 01:59:19 +0100

    date

--- help/build.sh
+++ help/build.sh
@@ -2,5 +2,7 @@ cd build && pdfcsplain index && mv index.pdf ../output && cd ..
 git add .
 git rm --cached index.pdf
 git commit -m "date"
+CHAR_COUNT=$(pdftotext output/index.pdf - | wc -m,)
 WORD_COUNT=$(pdftotext output/index.pdf - | wc -w)
 echo "WORD COUNT: $WORD_COUNT"
+echo "CHAR COUNT: $CHAR_COUNT"
