2d260358cda16ff620dc20e13e81bf04daa42268
Ondřej Tůma <tumaondra2000@gmail.com>
2022-01-10 01:41:25 +0100

    date

--- help/build.sh
+++ help/build.sh
@@ -2,4 +2,4 @@ cd build && pdfcsplain index && mv index.pdf ../output && cd ..
 git add .
 git rm --cached index.pdf
 git commit -m "date"
-pdftotext output/index.pdf - | wc -w
+LINE_COUNT=$(pdftotext output/index.pdf - | wc -w)
