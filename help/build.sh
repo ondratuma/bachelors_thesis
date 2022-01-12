fe354f9b890831d5323d4ed8dde2aa5c5cdf2431
Ondřej Tůma <tumaondra2000@gmail.com>
2022-01-12 23:42:10 +0100

    date

--- help/build.sh
+++ help/build.sh
@@ -1,8 +1,10 @@
+FIRST_PAGE_TO_COUNT=9
+
 cd build && pdfcsplain index && mv index.pdf ../output && cd ..
 git add .
 git rm --cached index.pdf
 git commit -m "date"
-CHAR_COUNT=$(pdftotext -f 9 output/index.pdf - | wc -m)
-WORD_COUNT=$(pdftotext -f 9 output/index.pdf - | wc -w)
+CHAR_COUNT=$(pdftotext -f $FIRST_PAGE_TO_COUNT output/index.pdf - | wc -m)
+WORD_COUNT=$(pdftotext -f $FIRST_PAGE_TO_COUNT output/index.pdf - | wc -w)
 echo "WORD COUNT: $WORD_COUNT"
 echo "CHAR COUNT: $CHAR_COUNT"
