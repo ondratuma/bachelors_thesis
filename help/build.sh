17e2459539177218f2ce166d5d5ab0a10a67ba69
Ondřej Tůma <tumaondra2000@gmail.com>
2022-01-13 00:53:15 +0100

    date

--- help/build.sh
+++ help/build.sh
@@ -5,7 +5,7 @@ cd build && pdfcsplain "$ROOT_FILE" && mv "${ROOT_FILE}.pdf" ../output/"${OUTPUT
 git add .
 git rm --cached "${OUTPUT_FILE}.pdf"
 git commit -m "date"
-TEXT=$(pdftotext "output/${OUTPUT_FILE}.pdf" -)
+TEXT=$(pdftotext "output/${OUTPUT_FILE}.pdf" - | grep -o 'Chapter 1.*' )
 CHAR_COUNT=$(echo $TEXT | wc -m)
 WORD_COUNT=$(echo $TEXT | cut -f1 -d"References" | wc -w)
 NORSMOSTRANY=$(echo "print(\"{:.2f}\".format(float(${CHAR_COUNT})/1800))" | python3)
