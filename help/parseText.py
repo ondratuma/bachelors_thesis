e4e5fae037ce71cbd7cfc149243a749d7d8411ef
Ondřej Tůma <tumaondra2000@gmail.com>
2022-01-13 01:30:01 +0100

    date

--- help/parseText.py
+++ help/parseText.py
@@ -3,10 +3,12 @@ import sys
 import re
 
 text = ""
+i = 0;
 for line in sys.stdin:
     if not re.search("https://.*", text):
         text += line
-
+        i+=1
+print(i)
 text = ''.join([i for i in text if i.isalpha()])
 text = ''.join(re.split("References", text)[:-1])
 text = ''.join(re.split("ChapterIntroduction", text)[1:])
