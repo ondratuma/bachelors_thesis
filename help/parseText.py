46fa5de43d3e249da1b394caff290c5d26d1fdf7
Ondřej Tůma <tumaondra2000@gmail.com>
2022-01-13 01:21:12 +0100

    date

--- help/parseText.py
+++ help/parseText.py
@@ -1,9 +1,11 @@
 
 import sys
- 
+import re
+
 text = ""
 for line in sys.stdin:
     text += line
 
 text = ''.join([i for i in text if i.isalpha()])
+text = re.split("Reference", txt)[:-1]
 print("Počet slov:",len(text))
