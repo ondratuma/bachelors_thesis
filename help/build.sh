cb68840f001aa598887854f05084ee08d15bbfec
Ondřej Tůma <tumaondra2000@gmail.com>
2022-01-13 06:40:46 +0100

    date

--- help/build.sh
+++ help/build.sh
@@ -40,7 +40,7 @@ echo "------------------------------------"
 echo "Celkem slov textu        : $NUMBER_OF_WORDS"
 echo "Celkem stran textu       : $TEXT_PAGES"
 echo "------------------------------------"
-echo "Znaků textu       : $NUMBER_OF_CHARS"
+echo "Znaků textu              : $NUMBER_OF_CHARS"
 echo "Normostrany       : $NORMOSTRAN"
 
 
