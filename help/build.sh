b48b79d1399fcecebd04d5374e6a6571fab4e706
Ondřej Tůma <tumaondra2000@gmail.com>
2022-01-13 04:49:24 +0100

    date

--- help/build.sh
+++ help/build.sh
@@ -12,14 +12,16 @@ NORSMOSTRANY=$(echo "$TEXT" | python help/parseText.py)
 echo "$NORSMOSTRANY"
 PAGE_COUNT_TOTAL=$(pdftk output/"${OUTPUT_FILE}.pdf" dump_data | grep NumberOfPages | awk -F ' ' '{print $2}')
 
-echo "První strana práce: $x"
-echo "Celkem stran: $PAGE_COUNT_TOTAL"
-echo "Celkem stran textu: $(( $PAGE_COUNT_TOTAL = + ))"
-
 while  [[ ! $PAGE_TEXT =~ .*Chapter.* ]]
 do
   PAGE_TEXT=$(pdftotext -f $x -l $x "output/${OUTPUT_FILE}.pdf" -)
   x=$(( $x + 1 ))
 done
+
+echo "První strana práce: $x"
+echo "Celkem stran: $PAGE_COUNT_TOTAL"
+echo "Celkem stran textu: $(( $PAGE_COUNT_TOTAL = $x ))"
+
+
 
 
