f65bd6a1880f63d56092f8bb2ed67897ebf0a582
Ondřej Tůma <tumaondra2000@gmail.com>
2022-01-13 01:00:30 +0100

    date

--- help/build.sh
+++ help/build.sh
@@ -6,6 +6,4 @@ git add .
 git rm --cached "${OUTPUT_FILE}.pdf"
 git commit -m "date"
 TEXT=$(pdftotext "output/${OUTPUT_FILE}.pdf" - | grep -o 'Chapter 1.*' )
-CHAR_COUNT=$(echo $TEXT | wc -m)
-WORD_COUNT=$(echo $TEXT | cut -f1 -d"References" | wc -w)
 NORSMOSTRANY=$(echo "$TEXT" | python3 parseText.py)
