5f2f3b90c507001e04873c361eda89c3799f68cc
Ondřej Tůma <tumaondra2000@gmail.com>
2022-01-13 01:13:05 +0100

    date

--- help/parseText.py
+++ help/parseText.py
@@ -2,5 +2,6 @@ file = open('output/text.txt',mode='r')
 all_of_it = file.read()
 
 chars = 0
-stripped = all_of_it.replace('\n', '').replace(' ', ''.strip()
+stripped = all_of_it.replace('\n', '').replace(' ', '').strip()
+
 print(len(stripped))
