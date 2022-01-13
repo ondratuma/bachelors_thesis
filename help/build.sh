66f94f502699d7f2d8cffca612f05bb29ff90b38
Ondřej Tůma <tumaondra2000@gmail.com>
2022-01-13 04:32:34 +0100

    date

--- help/build.sh
+++ help/build.sh
@@ -7,4 +7,4 @@ git commit -m "date"
 TEXT=$(pdftotext "output/${OUTPUT_FILE}.pdf" -)
 NORSMOSTRANY=$(echo "$TEXT" | python help/parseText.py)
 echo "$NORSMOSTRANY"
-exiftool output/"${OUTPUT_FILE}.pdf" | awk -F": " '/Page Count/{print $2}'
+pdftk output/"${OUTPUT_FILE}.pdf" dump_data | grep NumberOfPages
