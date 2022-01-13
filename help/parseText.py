31b8ffa6372c90dd649cea7eaa01751a020a9df4
Ondřej Tůma <tumaondra2000@gmail.com>
2022-01-13 01:24:46 +0100

    date

--- help/parseText.py
+++ help/parseText.py
@@ -9,5 +9,5 @@ for line in sys.stdin:
 text = ''.join([i for i in text if i.isalpha()])
 text = ''.join(re.split("References", text)[:-1])
 text = ''.join(re.split("ChapterIntroduction", text)[1:])
-print(text)
-print("Počet slov:",len(text))
+print("Počet znaků:",len(text))
+print("Počet normostran:",len(text)/1800)
