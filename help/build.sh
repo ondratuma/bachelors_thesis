b4accc68c8d68492586a1a742911f8c3b01a1649
Ondřej Tůma <tumaondra2000@gmail.com>
2022-01-13 01:00:12 +0100

    date

--- help/build.sh
+++ help/build.sh
@@ -8,10 +8,4 @@ git commit -m "date"
 TEXT=$(pdftotext "output/${OUTPUT_FILE}.pdf" - | grep -o 'Chapter 1.*' )
 CHAR_COUNT=$(echo $TEXT | wc -m)
 WORD_COUNT=$(echo $TEXT | cut -f1 -d"References" | wc -w)
-NORSMOSTRANY=$(echo "print(\"{:.2f}\".format(float(${CHAR_COUNT})/1800))" | python3)
-echo "---------------------------------------------------------------"
-echo "CONVERTED"
-echo "---------------------------------------------------------------"
-echo "WORD COUNT: $WORD_COUNT"
-echo "CHAR COUNT: $CHAR_COUNT"
-echo "Normostrany: $NORSMOSTRANY"
+NORSMOSTRANY=$(echo "$TEXT" | python3 parseText.py)
