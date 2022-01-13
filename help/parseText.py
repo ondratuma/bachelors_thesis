4d75c349c1be77cdf8bb39b18ee701e2c7747a20
Ondřej Tůma <tumaondra2000@gmail.com>
2022-01-13 01:24:12 +0100

    date

--- help/parseText.py
+++ help/parseText.py
@@ -8,5 +8,6 @@ for line in sys.stdin:
 
 text = ''.join([i for i in text if i.isalpha()])
 text = ''.join(re.split("References", text)[:-1])
+text = ''.join(re.split("ChapterIntroduction", text)[1:])
 print(text)
 print("Počet slov:",len(text))
