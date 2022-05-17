2f99d52bb2fe7a5b1a5889d80f0cd6c7864c1046
Ondřej Tůma <tumaondra2000@gmail.com>
2022-05-17 14:49:15 +0200

    date

--- help/build.sh
+++ help/build.sh
@@ -12,7 +12,7 @@ else
    OUTPUT_FILE=bakalarska_prace
 fi
 
-cd build && pdfcsplain -halt-on-error "$ROOT_FILE" && mv "${ROOT_FILE}.pdf" ../output/"${OUTPUT_FILE}.pdf" && cd ..
+cd build && pdfcsplain -halt-on-error "$ROOT_FILE" && mv "${ROOT_FILE}.pdf" ../output/temp.pdf && ps2pdf ../output/temp.pdf ../output/"$OUTPUT_FILE.pdf" && cd ..
 
 git add .
 git commit -m "date"
